dd0320190dd694857fa42cf6bae00c1e21170c2d
danilbro52 <danilsuper820a@gmailcom>
2024-07-27 12:55:28 +0200

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Versioning;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace MatrixSolution
+{
+    internal class Matrix
+    {
+        public List<List<double>> Values { get; set; } = new List<List<double>>();
+        public int Rows { get; set; }
+        public int Columns { get; set; }
+        public Matrix(string path)
+        {
+            try
+            {
+                StreamReader reader = new StreamReader(path);
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+                    List<double> row = new List<double>();
+                    line = line.Trim();
+                    for (int i = 0; i < line.Length; i++)
+                    {
+                        if (line[i] == ';')
+                        {
+                            row.Add(double.Parse(line.Remove(i)));
+                            line = line.Substring(i + 1);
+                            i = 0;
+                        }
+                    }
+                    Values.Add(row);
+                    Columns = row.Count;
+                }
+                reader.Close();
+                Rows = Values.Count;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Oops, you've got an error reading data from file: " + ex.Message);
+            }
+        }
+        public Matrix() { }
+        public void Transpose()
+        {
+            Matrix newOne = new Matrix();
+            for (int i = 0; i < Columns; i++)
+            {
+                List<double> hehe = new List<double>();
+                for (int j = 0; j < Rows; j++)
+                {
+                    hehe.Add(Values[j][i]);
+                }
+                newOne.Values.Add(hehe);
+            }
+            Values = newOne.Values;
+        }
+        public static Matrix operator +(Matrix a, Matrix b)
+        {
+            Matrix result = new Matrix();
+            if (a.Rows != b.Rows || a.Columns != b.Columns)
+            {
+                Console.WriteLine("ERROR! Matrixes should have same size (Rows and Columns)");
+                return new Matrix();
+            }
+            else
+            {
+                for (int i = 0; i < a.Rows; i++)
+                {
+                    List<double> val = new List<double>();
+                    for (int j = 0; j < a.Columns; j++)
+                    {
+                        double meow = a.Values[i][j] + b.Values[i][j];
+                        val.Add(meow);
+                    }
+                    result.Values.Add(val);
+                }
+            }
+            return result;
+        }
+        public static Matrix operator *(Matrix a, Matrix b)
+        {
+            if (a.Columns != b.Rows)
+            {
+                Console.WriteLine("ERROR! Matrixes can't be multiplicated!");
+                return new Matrix();
+            }
+            else
+            {
+                Matrix result = new Matrix();
+                for (int l = 0; l < a.Rows; l++)
+                {
+                    List<double> values = new List<double>();
+                    for (int i = 0; i < b.Columns; i++)
+                    {
+                        double number = 0;
+                        for (int j = 0; j < a.Columns; j++)
+                        {
+                            number += a.Values[l][j] * b.Values[j][i];
+                        }
+                        values.Add(number);
+                    }
+                    result.Values.Add(values);
+                }
+                return result;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,108 @@
+﻿    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection.Emit;
+    using System.Text;
+    using System.Threading.Tasks;
+
+namespace MatrixSolution
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //getting matrix from a file
+            string path = "C:\\Game Snake\\AI2\\AI z rozwójem\\x64\\Debug\\AI z rozwójem.tlog\\MatrixL\\matrix1.txt";
+            Matrix matrix1 = new Matrix(path); //getting matrix1 from a file
+            Console.WriteLine("First matrix:");
+            foreach (var meow in matrix1.Values)
+            {
+                foreach (var v in meow)
+                {
+                    Console.Write(v + "  ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            //Trasposing matrix1
+            matrix1.Transpose();
+            Console.WriteLine("Trasposing matrix 1:");
+            foreach (var meow in matrix1.Values)
+            {
+                foreach (var v in meow)
+                {
+                    Console.Write(v + "  ");
+                }
+                Console.WriteLine();
+            }
+
+            //getting matrix2 from a file
+            string path2 = "C:\\Game Snake\\AI2\\AI z rozwójem\\x64\\Debug\\AI z rozwójem.tlog\\MatrixL\\matrix2.txt";
+            Matrix matrix2 = new Matrix(path2);
+            Console.WriteLine();
+            Console.WriteLine("Second matrix:");
+            foreach (var meow in matrix2.Values)
+            {
+                foreach (var v in meow)
+                {
+                    Console.Write(v + "  ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            //Adding two matrices
+            Matrix summ = matrix1 + matrix2;
+            Console.WriteLine("Adding matrix 1 (after traspose) and matrix 2:");
+            foreach (var meow in summ.Values)
+            {
+                foreach (var v in meow)
+                {
+                    Console.Write(v + "  ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            //getting matrix 1 to multiply from file
+            Matrix matr_multpl1 = new Matrix("C:\\Game Snake\\AI2\\AI z rozwójem\\x64\\Debug\\AI z rozwójem.tlog\\MatrixL\\matrix_test1.txt");
+            Console.WriteLine("Getting matrix1 to multiply: ");
+            foreach (var meow in matr_multpl1.Values)
+            {
+                foreach (var v in meow)
+                {
+                    Console.Write(v + "  ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            //getting matrix 2 to multiply from file
+            Matrix matr_multpl2 = new Matrix("C:\\Game Snake\\AI2\\AI z rozwójem\\x64\\Debug\\AI z rozwójem.tlog\\MatrixL\\matrix_test2.txt");
+            Console.WriteLine("Getting matrix2 to multiply: ");
+            foreach (var meow in matr_multpl2.Values)
+            {
+                foreach (var v in meow)
+                {
+                    Console.Write(v + "  ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            //multiplying
+            Console.WriteLine("Multiplying matrix1 * matrix2:");
+            Matrix last = matr_multpl1 * matr_multpl2;
+            foreach (var meow in last.Values)
+            {
+                foreach (var v in meow)
+                {
+                    Console.Write(v + "  ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
